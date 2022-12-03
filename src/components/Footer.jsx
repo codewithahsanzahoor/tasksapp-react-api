09246c1fd4158fae1da7da5a0e551f990e394848
Muhammad Ahsan Zahoor <109317164+codewithahsanzahoor@users.noreply.github.com>
2022-12-03 22:59:46 +0500

    Footer styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,18 @@
 const Footer = () => {
 	return (
 		<footer>
-			<div className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top bg-black">
-				<p className="col-md-4 mb-0 text-muted">© 2021 Company, Inc</p>
+			<div
+				className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top bg-black"
+				style={{
+					position: 'absolute',
+					width: '100%',
+					top: '100vh',
+					textAlign: 'center',
+				}}
+			>
+				<p className="col-md-4 mb-0 text-muted" style={{ width: '100%' }}>
+					© 2021 Company, Inc
+				</p>
 				<a
 					href="/"
 					className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none"
